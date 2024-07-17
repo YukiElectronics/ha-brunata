5f19faa7ed4a5ee85526c77a00380c46974e80f9
Yuki Electronics <yuki@yukie.dev>
2024-07-17 10:42:19 +0200

    initial commit 🚀

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for Brunata Online integration."""
---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for Brunata Online tests."""
+from custom_components.brunata_online.const import (
+	CONF_PASSWORD,
+)
+from custom_components.brunata_online.const import (
+	CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
