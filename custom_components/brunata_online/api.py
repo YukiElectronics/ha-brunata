20c357afc68d842089fe9d83d4d5a87760a42f90
Yuki Electronics <yuki@yukie.dev>
2024-07-17 10:46:24 +0200

    fix(api): correct minor mistakes
    - Fixed mistaken renaming of parameters in requests
    - Temporarily moving from aiohttp to requests, as I haven't learned to use it properly yet
    - Disabled GHA "Run tests" action, as I have yet to attempt adding code coverage

--- custom_components/brunata_online/api.py
+++ custom_components/brunata_online/api.py
@@ -1,17 +1,14 @@
 """ Brunata Online API Client """
 
-import asyncio
 import base64
 from datetime import datetime, timedelta
 import hashlib
 import logging
 import os
 import re
-import socket
 import urllib.parse
 
-import aiohttp
-import async_timeout
+from requests import Session
 
 from .const import (
     B2C_URL,
@@ -23,24 +20,21 @@ from .const import (
     REDIRECT,
 )
 
-TIMEOUT = 10
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 
 
 class BrunataOnlineApiClient:
-    def __init__(
-        self, username: str, password: str, session: aiohttp.ClientSession
-    ) -> None:
+    def __init__(self, username: str, password: str) -> None:
         self._username = username
         self._password = password
-        self._session = session
+        self._session = Session()
         self._power = False
         self._water = False
         self._heating = False
         self._tokens = {}
         self._session.headers.update(HEADERS)
 
-    async def get_new_tokens(self) -> dict:
+    def get_new_tokens(self) -> None:
         # Check values
         if self._tokens:
             if (
@@ -56,38 +50,41 @@ class BrunataOnlineApiClient:
         # Initialize challenge values
         code_verifier = base64.urlsafe_b64encode(os.urandom(40)).decode("utf-8")
         code_verifier = re.sub("[^a-zA-Z0-9]+", "", code_verifier)
-
         code_challenge = hashlib.sha256(code_verifier.encode("utf-8")).digest()
-        code_challenge = base64.urlsafe_b64encode(code_challenge).decode("utf-8")
-        code_challenge = code_challenge.replace("=", "")
-
+        code_challenge = (
+            base64.urlsafe_b64encode(code_challenge).decode("utf-8").replace("=", "")
+        )
         # Initial authorization call
         req_code = self.api_wrapper(
             "GET",
             url=f"{BASE_URL.replace('webservice', 'auth-webservice')}/authorize",
             params={
-                "CLIENT_ID": CLIENT_ID,
-                "REDIRECT_URL": REDIRECT,
+                "client_id": CLIENT_ID,
+                "redirect_uri": REDIRECT,
                 "scope": f"{CLIENT_ID} offline_access",
                 "response_type": "code",
                 "code_challenge": code_challenge,
                 "code_challenge_method": "S256",
             },
         )
+        print(req_code)
         # Get CSRF Token & Transaction ID
-        csrf_token = req_code.cookies.get("x-ms-cpim-csrf")
+        try:
+            csrf_token = req_code.cookies.get("x-ms-cpim-csrf")
+        except Exception as exception:
+            _LOGGER.error("Error while retrieving CSRF Token: %s", exception)
+            return
         match = re.search(r"var SETTINGS = (\{[^;]*\});", req_code.text)
         if match:  # Use a little magic to avoid proper JSON parsing ✨
             transId = [
                 i for i in match.group(1).split('","') if i.startswith("transId")
             ][0][10:]
             _LOGGER.debug("Transaction ID: %s", transId)
-            _LOGGER.debug("Transaction ID TEST: {}", transId)
         else:
             _LOGGER.error("Failed to get Transaction ID")
             return
         # Post credentials to B2C Endpoint
-        self.api_wrapper(
+        req_auth = self.api_wrapper(
             "POST",
             url=f"{B2C_URL}/SelfAsserted",
             params={
@@ -124,13 +121,13 @@ class BrunataOnlineApiClient:
             "code"
         ][0]
         # Get OAuth 2.0 token object
-        result = await self.api_wrapper(
+        result = self.api_wrapper(
             "POST",
             url=f"{OAUTH2_URL}/token",
             data={
                 "grant_type": "authorization_code",
-                "CLIENT_ID": CLIENT_ID,
-                "REDIRECT_URL": REDIRECT,
+                "client_id": CLIENT_ID,
+                "redirect_uri": REDIRECT,
                 "code": auth_code,
                 "code_verifier": code_verifier,
             },
@@ -138,13 +135,12 @@ class BrunataOnlineApiClient:
         # Add access token to session headers
         self._session.headers.update(
             {
-                "Authorization": f"Bearer {result.get('access_token')}",
+                "Authorization": f"{result.get('token_type')} {result.get('access_token')}"
             }
         )
         self._tokens = result
-        return result
 
-    async def renew_tokens(self) -> dict | None:
+    def renew_tokens(self) -> dict | None:
         # Check if token is expired
         if not self._tokens:
             _LOGGER.error("Attempted to renew tokens when no tokens are stored")
@@ -162,7 +158,7 @@ class BrunataOnlineApiClient:
             else:  # TODO: Implement check of refresh_token expiry
                 _LOGGER.warn("Refresh token expiry check not implemented yet")
         # Get OAuth 2.0 token object
-        tokens = await self.api_wrapper(
+        tokens = self.api_wrapper(
             "POST",
             url=f"{OAUTH2_URL}/token",
             data={
@@ -174,14 +170,14 @@ class BrunataOnlineApiClient:
         if tokens.get("access_token"):
             self._session.headers.update(
                 {
-                    "Authorization": f"Bearer {tokens.get('access_token')}",
+                    "Authorization": f"{tokens.get('token_type')} {tokens.get('access_token')}",
                 }
             )
             self._tokens = tokens
         return tokens or None
 
-    async def get_available_meters(self) -> dict:
-        meters = await self.api_wrapper(
+    def get_available_meters(self) -> dict:
+        meters = self.api_wrapper(
             "GET",
             url=f"{BASE_URL}/consumer/superallocationunits",
             headers={
@@ -203,23 +199,23 @@ class BrunataOnlineApiClient:
             self._heating = True
         return meters or {}
 
-    def start_of_month():
+    def start_of_month(self) -> str:
         first_day = datetime.utcnow().replace(
             day=1, hour=0, minute=0, second=0, microsecond=0
         )
         return f"{first_day.isoformat()}.000Z"
 
-    def end_of_month() -> str:
+    def end_of_month(self) -> str:
         end = datetime.utcnow().replace(day=28) + timedelta(days=4)
         end -= timedelta(days=end.day)
         end = end.replace(hour=23, minute=59, second=59, microsecond=0)
         return f"{end.isoformat()}.999Z"
 
-    async def get_monthly_energy(self) -> dict | None:
+    def get_monthly_energy(self) -> dict | None:
         if not self._power:
             _LOGGER.debug("No power meter was found 🌃")
             return
-        energy = await self.api_wrapper(
+        energy = self.api_wrapper(
             "GET",
             url=f"{BASE_URL}/consumer/consumption",
             params={
@@ -235,54 +231,17 @@ class BrunataOnlineApiClient:
         _LOGGER.info("This month's daily energy consumption:")
         for e in energy["consumptionLines"][0]["consumptionValues"]:
             if e.get("consumption") is not None:
-                _LOGGER.info(
-                    "%s: %s",
-                    datetime.fromisoformat(e.get("fromDate")).strftime("%B %d, %Y"),
-                    e.get("consumption"),
-                )
+                _LOGGER.info("%s: %s kWh", e.get("fromDate")[:10], e.get("consumption"))
         return energy or {}
 
-    async def api_wrapper(
-        self,
-        method: str,
-        url: str,
-        params: dict = {},
-        data: dict = {},
-        headers: dict = {},
-    ) -> dict:
+    def api_wrapper(self, method: str, **args) -> dict:
         """Get information from the API."""
-        try:
-            async with async_timeout.timeout(TIMEOUT, loop=asyncio.get_event_loop()):
-                match method:
-                    case "GET":
-                        return await self._session.get(url, headers=headers)
-                    case "POST":
-                        return await self._session.post(url, headers=headers, data=data)
-                    case "PUT":
-                        return await self._session.put(url, headers=headers, data=data)
-                    case "PATCH":
-                        return await self._session.patch(
-                            url, headers=headers, data=data
-                        )
-
-        except asyncio.TimeoutError as exception:
-            _LOGGER.error(
-                "Timeout error fetching information from %s - %s",
-                url,
-                exception,
-            )
-
-        except (KeyError, TypeError) as exception:
-            _LOGGER.error(
-                "Error parsing information from %s - %s",
-                url,
-                exception,
-            )
-        except (aiohttp.ClientError, socket.gaierror) as exception:
-            _LOGGER.error(
-                "Error fetching information from %s - %s",
-                url,
-                exception,
-            )
-        except Exception as exception:  # pylint: disable=broad-except
-            _LOGGER.error("Something really wrong happened! - %s", exception)
+        match method:
+            case "GET":
+                return self._session.get(**args)
+            case "POST":
+                return self._session.post(**args)
+            case "PUT":
+                return self._session.put(**args)
+            case "PATCH":
+                return self._session.patch(**args)
