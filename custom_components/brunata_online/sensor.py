2918301613689f23f74053202cdcfa06a3f84d6a
Yuki Electronics <yuki@yukie.dev>
2024-07-18 16:40:24 +0200

    feat(sensor): add preliminary classes

--- custom_components/brunata_online/sensor.py
+++ custom_components/brunata_online/sensor.py
@@ -1,5 +1,8 @@
 """Sensor platform for Brunata Online."""
 
+from homeassistant.components.sensor import SensorDeviceClass, SensorStateClass
+from homeassistant.const import UnitOfEnergy, UnitOfVolume
+
 from .const import DEFAULT_NAME, DOMAIN, ICON, SENSOR
 from .entity import BrunataOnlineEntity
 
@@ -7,11 +10,18 @@ from .entity import BrunataOnlineEntity
 async def async_setup_entry(hass, entry, async_add_devices):
     """Setup sensor platform."""
     coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_devices([BrunataOnlineSensor(coordinator, entry)])
+    async_add_devices([BrunataOnlineEnergySensor(coordinator, entry)])
+    async_add_devices([BrunataOnlineWaterSensor(coordinator, entry)])
+    async_add_devices([BrunataOnlineHeatingSensor(coordinator, entry)])
 
 
-class BrunataOnlineSensor(BrunataOnlineEntity):
-    """brunata_online Sensor class."""
+class BrunataOnlineEnergySensor(BrunataOnlineEntity):
+    """Energy Sensor"""
+
+    _attr_name = "Brunata Energy Consumed"
+    _attr_native_unit_of_measurement = UnitOfEnergy
+    _attr_device_class = SensorDeviceClass.ENERGY
+    _attr_state_class = SensorStateClass.TOTAL_INCREASING
 
     @property
     def name(self):
@@ -30,5 +40,63 @@ class BrunataOnlineSensor(BrunataOnlineEntity):
 
     @property
     def device_class(self):
-        """Return de device class of the sensor."""
+        """Return the device class of the sensor."""
+        return "brunata_online__custom_device_class"
+
+
+class BrunataOnlineWaterSensor(BrunataOnlineEntity):
+    """brunata_online Sensor class."""
+
+    _attr_name = "Brunata Water Consumed"
+    _attr_native_unit_of_measurement = UnitOfVolume
+    _attr_device_class = SensorDeviceClass.VOLUME
+    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{DEFAULT_NAME}_{SENSOR}"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self.coordinator.data.get("body")
+
+    @property
+    def icon(self):
+        """Return the icon of the sensor."""
+        return ICON
+
+    @property
+    def device_class(self):
+        """Return the device class of the sensor."""
+        return "brunata_online__custom_device_class"
+
+
+class BrunataOnlineHeatingSensor(BrunataOnlineEntity):
+    """brunata_online Sensor class."""
+
+    _attr_name = "Brunata Energy Consumed"
+    _attr_native_unit_of_measurement = UnitOfEnergy
+    _attr_device_class = SensorDeviceClass.ENERGY
+    _attr_state_class = SensorStateClass.TOTAL_INCREASING
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{DEFAULT_NAME}_{SENSOR}"
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self.coordinator.data.get("body")
+
+    @property
+    def icon(self):
+        """Return the icon of the sensor."""
+        return ICON
+
+    @property
+    def device_class(self):
+        """Return the device class of the sensor."""
         return "brunata_online__custom_device_class"
